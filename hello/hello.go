ccb0504de55295d60009507b9cb3b87d1bcf02e4
i515190 <jacob.xi@sap,com>
2019-09-08 18:42:13 +0800

    add files to master branch

---
+++ hello/hello.go
@@ -0,0 +1,8 @@
+package hello
+
+func Greet() string {
+	return "Hello Github Action"
+}
+
+//  [Creating packages as function libraries]
+// (https://github.com/Chao-Xi/JacobTechBlog/blob/master/golang/7Go_Struc_code.md#3creating-packages-as-function-libraries)
